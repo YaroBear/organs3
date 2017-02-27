893b800700735fe8217eb94c67c827ae2db27735
YaroBear <berejnoi.yaroslav@gmail.com>
2017-02-27 13:09:33 -0600

    Made init_collections an export module with two functions: buildCollections() and destroyCollections()

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ var mongodb = require("mongodb");
 
 var app = express();
 
+//importing init_collections.js
+var collections = require("./init_collections");
+
+collections.buildCollections();
+//collections.destroyCollections();
