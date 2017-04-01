944fc59789b5224d87fff5b6606fa6a9f86b414a
hasnainbilgrami <hrbilgra@gmail.com>
2017-04-01 14:09:47 -0500

    frontend for statsdone

--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,10 @@ var router = express.Router();
 //authentication
 var app = express();
 var jwt = require('jsonwebtoken');
+
+//plotly 
+var plotly = require('plotly')("hasnainbilgrami", "Tp0ci7oUZdLrQ5Dg3AdZ")
+
 //var config = require('./config');
 app.set('superSecret', process.env.SECRET);
 
@@ -19,44 +23,44 @@ mongoose.connect(mongodb_uri);
 
 // models
 var doctorSchema = new Schema({
-	name : {
-		type: String,
-		required: [true, "A name is required"]
-	},
-	patients : []
+    name: {
+        type: String,
+        required: [true, "A name is required"]
+    },
+    patients: []
 });
 
 
 var userSchema = new Schema({
-	ssn : {
-		type: String,
-		unique: true,
-		validate: {
-			validator: function(v) {
-				return /\d{3}-\d{2}-\d{4}/.test(v);
-			},
-			message: "Please enter your SSN as xxx-xx-xxxx"
-		},
-		required: [true, "Social Security number required"]
-	},
-	name: {type: String, required: [true, "Name is required"]},
-	username : {type: String, required: [true, "Username is required"], unique: true},
-	password : {type: String, required: [true, "Password is required"]},
-	admin : Boolean
+    ssn: {
+        type: String,
+        unique: true,
+        validate: {
+            validator: function(v) {
+                return /\d{3}-\d{2}-\d{4}/.test(v);
+            },
+            message: "Please enter your SSN as xxx-xx-xxxx"
+        },
+        required: [true, "Social Security number required"]
+    },
+    name: { type: String, required: [true, "Name is required"] },
+    username: { type: String, required: [true, "Username is required"], unique: true },
+    password: { type: String, required: [true, "Password is required"] },
+    admin: Boolean
 });
 
 var hospitalSchema = new Schema({
-	name: String,
-	address : {
-		street : String,
-		city: String,
-		state: String,
-		zip: String,
-		region: Number
-	},
-	phone : String,
-	procedures : Array,
-	doctors : Array
+    name: String,
+    address: {
+        street: String,
+        city: String,
+        state: String,
+        zip: String,
+        region: Number
+    },
+    phone: String,
+    procedures: Array,
+    doctors: Array
 });
 
 
@@ -64,24 +68,24 @@ var hospitalSchema = new Schema({
 
 var Hospitals = mongoose.model('hospitals', hospitalSchema);
 
-router.get('/api/hospitals/names', function(req, res){
-	Hospitals.find({}, {name: 1}, function(err, data){
-		if (err)
-			res.send(err);
-		else
-			res.json(data);
-	});
+router.get('/api/hospitals/names', function(req, res) {
+    Hospitals.find({}, { name: 1 }, function(err, data) {
+        if (err)
+            res.send(err);
+        else
+            res.json(data);
+    });
 });
 
 var Doctors = mongoose.model('doctors', doctorSchema);
 
 router.get('/api/doctors', function(req, res) {
-	Doctors.find(function(err, data){
-		if(err)
-			res.send(err);
-		else
-			res.json(data);
-	});
+    Doctors.find(function(err, data) {
+        if (err)
+            res.send(err);
+        else
+            res.json(data);
+    });
 });
 
 router.post('/api/doctors', function(req, res) {
@@ -90,89 +94,79 @@ router.post('/api/doctors', function(req, res) {
     var request = {};
     // if req.body is empty (form is empty), use query parameters 
     // to test API without front end via Postman or regular xmlhttprequest
-    if (Object.keys(req.body).length === 0 && req.body.constructor === Object)
-    {    
+    if (Object.keys(req.body).length === 0 && req.body.constructor === Object) {
         console.log("using req.query");
         request = req.query;
-    }
-    else
-    {
+    } else {
         console.log("using req.body")
         request = req.body;
     }
 
     var errors = {};
 
-    User.findOne({ssn : request.SSN})
+    User.findOne({ ssn: request.SSN })
         .then(function(ssn) {
-            if (ssn)
-            {
+            if (ssn) {
                 errors.ssnExists = "A user with that SSN already exists";
             }
-            return User.findOne({username : request.username});
+            return User.findOne({ username: request.username });
         }).then(function(username) {
-            if (username)
-            {
+            if (username) {
                 errors.usernameExists = "A user with that username already exists";
             }
-        }).then(function(){
+        }).then(function() {
             var isAdmin = false;
-            if (request.code == undefined)
-            {
+            if (request.code == undefined) {
                 errors.permCode = "A system permission code is required";
-            }
-            else if (request.code == "1234")
-            {
+            } else if (request.code == "1234") {
                 isAdmin = true;
             }
 
             // create a new user
             var newUser = User({
-                ssn : request.SSN,
-                name : request.name,
-                username : request.username,
-                password : request.password,
-                admin : isAdmin
+                ssn: request.SSN,
+                name: request.name,
+                username: request.username,
+                password: request.password,
+                admin: isAdmin
             });
 
             return newUser.validate().then(function() { return newUser; });
         }).catch(function(err) {
             errors.validationError = err;
         }).then(function(newUser) {
-            if(errors.validationError || errors.permCode) {
+            if (errors.validationError || errors.permCode) {
                 var error = {};
                 error.message = 'Failed to add user';
                 error.code = 400;
                 error.errors = errors;
                 throw error;
+            } else {
+                return newUser.save().then(function() { return newUser; });
             }
-            else
-            {
-            	return newUser.save().then(function() {return newUser;});
-            }
-        }).catch(function(err){
-    	    var error = {};
+        }).catch(function(err) {
+            var error = {};
             error.message = err.message;
             error.code = 400;
             error.errors = errors;
             throw error;
         }).then(function(newUser) {
-        	var newDoctor = new Doctors({
-        		_id : newUser._id,
-        		name : newUser.name
-        	});
+            var newDoctor = new Doctors({
+                _id: newUser._id,
+                name: newUser.name
+            });
 
-        	return newDoctor.save().then(function() {return newUser;});
-        
+            return newDoctor.save().then(function() { return newUser; });
+
         }).then(function(newUser) {
-        	Hospitals.findOneAndUpdate({"_id": request.selectedHospital._id}, {$push:{doctors: newUser._id}}).then(function() {return Hospitals});
-        }).then(function(Hospitals){
-            res.status(201).send({ok: true, message: 'Added user successfully'});
+            Hospitals.findOneAndUpdate({ "_id": request.selectedHospital._id }, { $push: { doctors: newUser._id } }).then(function() { return Hospitals });
+        }).then(function(Hospitals) {
+            res.status(201).send({ ok: true, message: 'Added user successfully' });
         }).catch(function(err) {
-			var errorCode = err.code || 500;
-            res.status(errorCode).send({ok: false, message: err.message, errors: err.errors});
+            var errorCode = err.code || 500;
+            res.status(errorCode).send({ ok: false, message: err.message, errors: err.errors });
         });
-    });
+});
 
 //user authentication
 router.post('/api/authenticate', function(req, res) {
@@ -181,42 +175,39 @@ router.post('/api/authenticate', function(req, res) {
     var request = {};
     // if req.body is empty (form is empty), use query parameters 
     // to test API without front end via Postman or regular xmlhttprequest
-    if (Object.keys(req.body).length === 0 && req.body.constructor === Object)
-    {   
+    if (Object.keys(req.body).length === 0 && req.body.constructor === Object) {
         console.log("using req.query");
         request = req.query;
-    }
-    else
-    {
+    } else {
         console.log("using req.body");
         request = req.body;
     }
 
     console.log(request.username);
 
-    if (request.username != null){
+    if (request.username != null) {
 
         User.findOne({
             username: request.username
         }, function(err, user) {
 
             //console.log(user.username);
-            if(err) throw err;
+            if (err) throw err;
 
-            if(!user){
+            if (!user) {
                 res.json({ success: false, message: 'Authentication failed. User not found.' });
             } else if (user) {
 
                 //check password
-                if (user.password != request.password){
+                if (user.password != request.password) {
                     res.json({ success: false, message: 'Authentication failed. Wrong password.' });
                 } else {
                     //if user is found and password is correct generate token
                     var token = jwt.sign(user, app.get('superSecret'), {
-                        expiresIn: 60*60
+                        expiresIn: 60 * 60
                     });
 
-                    if (user.admin){                    
+                    if (user.admin) {
 
                         //return message and token
                         res.json({
@@ -224,8 +215,7 @@ router.post('/api/authenticate', function(req, res) {
                             message: 'Admin Login successful',
                             token: token
                         });
-                    }
-                    else{
+                    } else {
                         //return message and token
                         res.json({
                             success: true,
@@ -237,10 +227,9 @@ router.post('/api/authenticate', function(req, res) {
                 }
 
             }
-            
+
         });
-    }
-    else {
+    } else {
         res.send("Please enter a username");
     }
 });
@@ -250,19 +239,35 @@ router.post('/api/authenticate', function(req, res) {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+    res.render('index');
 });
 
 /* GET register page. */
 router.get('/register', function(req, res, next) {
-  res.render('register');
+    res.render('register');
 });
 
 
 /* GET login page. */
 router.get('/authenticate', function(req, res, next) {
-  res.render('authenticate');
+    res.render('authenticate');
 });
+
+router.get('/statistics', function(req, res, next) {
+    // var data = [{ x: [0, 1, 2], y: [3, 2, 1], type: 'bar' }];
+    // var layout = { fileopt: "overwrite", filename: "simple-node-example" };
+
+    // var data = [{
+    //     x: ["2013-10-04 22:23:00", "2013-11-04 22:23:00", "2013-12-04 22:23:00"],
+    //     y: [1, 3, 6],
+    //     type: "scatter"
+    // }];
+    // var graphOptions = { filename: "statisticsMatching", fileopt: "overwrite" };
+    // plotly.plot(data, graphOptions, function(err, msg) {
+    //     console.log(msg);
+    // });
+    res.render('statisticsMatching');
+})
 
 
 
@@ -271,33 +276,33 @@ router.get('/authenticate', function(req, res, next) {
 //before any protected routes
 router.use(function(req, res, next) {
 
-  // check header or url parameters or post parameters for token
-  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
-  // decode token
-  if (token) {
+    // decode token
+    if (token) {
 
-    // verifies secret and checks exp
-    jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
-      if (err) {
-        return res.json({ success: false, message: 'Failed to authenticate token.' });    
-      } else {
-        // if everything is good, save to request for use in other routes
-        req.decoded = decoded;    
-        next();
-      }
-    });
+        // verifies secret and checks exp
+        jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+            if (err) {
+                return res.json({ success: false, message: 'Failed to authenticate token.' });
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                next();
+            }
+        });
 
-  } else {
+    } else {
 
-    // if there is no token
-    // return an error
-    return res.status(403).send({ 
-        success: false, 
-        message: 'No token provided.' 
-    });
-    
-  }
+        // if there is no token
+        // return an error
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+
+    }
 });
 
 
@@ -305,7 +310,7 @@ router.use(function(req, res, next) {
 
 /* GET register page. */
 router.get('/admin', function(req, res, next) {
-  res.render('admin');
+    res.render('admin');
 });
 
 
